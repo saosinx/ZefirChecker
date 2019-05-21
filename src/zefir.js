ba1a24778a98b119128ea55cd82c0940fdf3695c
anberlin <zhurawski-nikita@gmail.com>
2019-05-22 00:07:38 +0300

    develop

--- src/zefir.js
+++ src/zefir.js
@@ -1,49 +1,79 @@
 function setListOption(list, string) {
-  const children = list.children.length;
+	const children = list.children.length
 
-  for (let i = 0; i < children; i += 1) {
-    const child = list.children[i];
-    const childString = child.innerHTML.toLowerCase();
+	for (let i = 0; i < children; i += 1) {
+		const child = list.children[i]
+		const childString = child.innerHTML.toLowerCase()
 
-    if (childString.includes(string)) {
-      list.value = child.value;
+		if (childString.includes(string)) {
+			list.value = child.value
 
-      list.classList.remove("zefirSuccess");
-      void list.offsetWidth;
-      list.classList.add("zefirSuccess");
-    }
-  }
+			list.classList.remove('zefirSuccess')
+			void list.offsetWidth
+			list.classList.add('zefirSuccess')
+		}
+	}
 }
 
-chrome.runtime.onMessage.addListener(request => {
-  const list = document.getElementById("cycle_version") || null;
-  const string = request.string.length ? request.string.toLowerCase() : null;
+chrome.runtime.onMessage.addListener((request) => {
+	const list = document.getElementById('cycle_version') || null
+	const string = request.string.length ? request.string.toLowerCase() : null
 
-  if (list && string) {
-    setListOption(list, string);
-  }
-});
+	if (list && string) {
+		setListOption(list, string)
+	}
+})
 
-console.log("DOMContentLoaded");
+console.log('%cDOMContentLoaded', 'color: salmon;')
 
-const zefirSelect = document.getElementById("zefir-select");
-const zefirInput = zefirSelect.querySelector(".zefir-select-search-input");
-const zefirDropdown = zefirSelect.querySelector(".zefir-select-dropdown");
-const zefirArrowIcon = zefirSelect.querySelector(".zefir-select-arrow");
-console.log(zefirInput);
+const zefirMain = document.getElementById('cycle_version') || null
 
-zefirSelect.addEventListener("click", function() {
-  this.classList.add("zefir-select-container_focused");
-  this.classList.add("zefir-select-container_opened");
+const zefirSelect = document.getElementById('zefir-select')
+const zefirInput = zefirSelect.querySelector('.zefir-select-search-input')
+const zefirSelectedValue = zefirSelect.querySelector('.zefir-select-selected-value')
+const zefirDropdown = zefirSelect.querySelector('.zefir-select-dropdown')
+const zefirArrowIcon = zefirSelect.querySelector('.zefir-select-arrow')
 
-  zefirInput.focus()
-});
+for (let i = 0; i < zefirMain.options.length; i += 1) {
+	const elem = document.createElement('li')
 
-document.addEventListener("click", function(e) {
-  if (!e.target.closest("#zefir-select")) {
-    zefirSelect.classList.remove(
-      "zefir-select-container_focused",
-      "zefir-select-container_opened"
-    );
-  }
-});
+	elem.innerHTML = zefirMain.options[i].innerHTML
+	elem.setAttribute('value', zefirMain.options[i].value)
+
+	zefirDropdown.children[0].appendChild(elem)
+}
+
+zefirInput.addEventListener('input', (e) => {
+	zefirInput.value.length
+		? (zefirSelectedValue.style.display = 'none')
+		: (zefirSelectedValue.style.display = 'block')
+})
+
+zefirSelect.addEventListener('click', (e) => {
+	if (e.target.closest('.zefir-select-arrow')) {
+		zefirSelect.classList.contains('--focused') ? '' : zefirSelect.classList.add('--focused')
+		zefirSelect.classList.toggle('--opened')
+		zefirSelect.classList.contains('--opened') ? zefirInput.focus() : zefirInput.blur()
+		return
+	}
+
+	if (e.target.tagName === 'LI') {
+		zefirSelect.classList.remove('--opened')
+		zefirInput.blur()
+
+		zefirSelectedValue.innerHTML = e.target.innerHTML
+		zefirSelectedValue.setAttribute('title', e.target.innerHTML)
+
+		return
+	}
+
+	zefirSelect.classList.add('--focused')
+	zefirSelect.classList.add('--opened')
+	zefirInput.focus()
+})
+
+document.addEventListener('click', (e) => {
+	if (!e.target.closest('#zefir-select')) {
+		zefirSelect.classList.remove('--focused', '--opened')
+	}
+})
