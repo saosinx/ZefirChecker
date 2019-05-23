778c586fc17241dad83990ca468ecc5aca72112c
nikita.zhuravskiy <zhurawski.nikita@gmail.com>
2019-05-23 10:31:35 +0300

    review version_1

--- src/zefir.js
+++ src/zefir.js
@@ -18,8 +18,8 @@ function activateZefir() {
 			zefirDropdown.appendChild(elem)
 
 			if (zefirList.value === zefirList.options[i].value) {
-				zefirSelectedValue.setAttribute('value', zefirList.value)
-				zefirSelectedValue.innerHTML = zefirList.options[i].innerHTML
+				zefirValue.setAttribute('value', zefirList.value)
+				zefirValue.innerHTML = zefirList.options[i].innerHTML
 			}
 		}
 	}
@@ -34,7 +34,7 @@ function activateZefir() {
 		force ? cb() : setTimeout(cb, 300)
 
 		zefirInput.value = ''
-		zefirSelectedValue.style.display = 'block'
+		zefirValue.style.display = 'block'
 	}
 
 	const setZefirSelectPosition = function() {
@@ -49,23 +49,57 @@ function activateZefir() {
 
 		zefirList.value = option.getAttribute('value')
 
-		zefirSelectedValue.innerHTML = option.innerHTML
-		zefirSelectedValue.setAttribute('value', option.getAttribute('value'))
+		zefirValue.innerHTML = option.innerHTML
+		zefirValue.setAttribute('value', option.getAttribute('value'))
 	}
 
-	const zefirSelect = document.getElementById('zefir-select')
+	const generateZefirSelectMarkup = function() {
+		elem = document.createElement('div')
+		elem.setAttribute('id', 'zefir-select')
+		elem.setAttribute('class', 'zefir-select-container');
+		elem.innerHTML = `
+			<div class="zefir-select" tabindex="0">
+				<div class="zefir-select-inner">
+					<div class="zefir-select-selected-value" value=""></div>
+					<div class="zefir-select-search">
+						<div class="zefir-select-search-inner">
+							<input class="zefir-select-search-input" autocomplete="off" value="" />
+							<span class="zefir-select-search-mirror">&nbsp;</span>
+						</div>
+					</div>
+				</div>
+				<span class="zefir-select-arrow" unselectable="on">
+					<i class="zefiricon zefir-select-arrow-icon">
+						<svg viewbox="64 64 896 896" width="1em" height="1em" fill="currentColor" focusable="false">
+							<path d="M884 256h-75c-5.1 0-9.9 2.5-12.9 6.6L512 654.2 227.9 262.6c-3-4.1-7.8-6.6-12.9-6.6h-75c-6.5 0-10.3 7.4-6.5 12.7l352.6 486.1c12.8 17.6 39 17.6 51.7 0l352.6-486.1c3.9-5.3.1-12.7-6.4-12.7z"></path>
+						</svg>
+					</i>
+				</span>
+			</div>
+			<div class="zefir-select-dropdown">
+				<ul class="zefir-select-dropdown-list"></ul>
+			</div>
+		`
+
+		return elem
+	}
+
+	const zefirSelect = generateZefirSelectMarkup()
+
+	document.body.appendChild(zefirSelect)
+
 	const zefirInput = zefirSelect.querySelector('.zefir-select-search-input')
-	const zefirSelectedValue = zefirSelect.querySelector('.zefir-select-selected-value')
+	const zefirValue = zefirSelect.querySelector('.zefir-select-selected-value')
 	const zefirDropdown = zefirSelect.querySelector('.zefir-select-dropdown-list')
 
 	fillDropdownMenu()
 
-	setTimeout(setZefirSelectPosition, 50)
+	setTimeout(setZefirSelectPosition, 100)
 
 	zefirInput.addEventListener('input', (e) => {
 		zefirInput.value.length
-			? (zefirSelectedValue.style.display = 'none')
-			: (zefirSelectedValue.style.display = 'block')
+			? (zefirValue.style.display = 'none')
+			: (zefirValue.style.display = 'block')
 
 		if (zefirInput.value.length) {
 			;[].forEach.call(zefirDropdown.children, (child) => {
